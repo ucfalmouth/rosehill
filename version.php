025463cd7883dfe2aa9436da64b43de7ba70bd1a
Simon Ferris <simon.ferris@falmouth.ac.uk>
2023-07-04 10:30:12 +0100

    Updated version/release for prod

--- version.php
+++ version.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -25,14 +26,14 @@
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2023032800';
+$plugin->version = '2023062300';
 $plugin->requires = '2022112800';
 // $plugin->supported = TODO;   // Available as of Moodle 3.9.0 or later.
 // $plugin->incompatible = TODO;   // Available as of Moodle 3.9.0 or later.
 $plugin->component = 'theme_rosehill';
 // $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.0.0';
+$plugin->release = 'v1.1.0';
 
 $plugin->dependencies = [
-	'theme_boost' => '2022112800'
+    'theme_boost' => '2022112800'
 ];
